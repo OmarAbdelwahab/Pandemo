4e9eddd06892ccf1d635efeb26c9794d79f6a6b7
Omar Abdelwahab <omar.abdelwahab.info@gmail.com>
2020-09-15 00:30:52 +0200

    using the async fetch data function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+
+    console.log(fetchedData);
+  }
+
   render() {
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={this.state.data} />
         <CountryPicker />
         <Chart />
       </div>
